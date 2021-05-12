7229a8c2c148b5e0df8ad38f7f02781bfafcdd9f
Batuhan Balı <balibatuhan19@gmail.com>
2021-05-12 12:30:06 +0300

    Adding user api's

---
+++ hrms/src/main/java/kodlamaio/hrms/entities/concretes/User.java
@@ -0,0 +1,30 @@
+package kodlamaio.hrms.entities.concretes;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@Table(name= "users")
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+	
+	@Id
+	@GeneratedValue
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="email")
+	private String email;
+	
+	@Column(name="password")
+	private String password;
+}
